bbb376ab4301568728f13ba56070c463da21290b
thelmaijemma <okoli.thelma@gmail.com>
2021-01-19 02:10:45 +0000

    Initial files for sidebar navigation

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {Component } from 'react';
+import MainSidebar from './MainSidebar';
+import { Route  } from 'react-router-dom';
+import SideBar from './SideBar';
+import Header from './Header';
 
-function App() {
+class App extends Component() {
+  render(){
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div>
+      <Header></Header>
+    <SideBar>
+      <Route path='/' component={MainSidebar} />
+    </SideBar>
+  </div>
   );
+  }
 }
 
 export default App;
+
+
+{/*    <Sidebar>
+      <Route path='/' component={MainSidebar} />
+      <Route path='/folder' component={SidebarFolders} />
+      <Route path='/note' component={SidebarNotes} />
+    </Sidebar>
+    <Main>
+      <Route path='/' component={MainMain} />
+      <Route path='/folder' component={MainFolders} />
+      <Route path='/note' component={MainNotes} />
+    </Main> */}
