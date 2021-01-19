ff61ab77071e0de2b59bda5a3f8f91b3cf6b1e7f
thelmaijemma <okoli.thelma@gmail.com>
2021-01-19 22:54:51 +0000

    added render to App router

---
+++ src/Notes.js
@@ -0,0 +1,28 @@
+import React, {Component } from 'react';
+import FOLDERS from './Folders';
+import { Link } from 'react-router-dom';
+
+
+
+const notes = FOLDERS.notes;
+
+class Notes extends Component {
+    render(){
+        return (
+            <div className="NoteSet">
+            {notes.map(note => {
+                return <li>
+                    <Link
+                to={`/note/${note.id}`}
+                >
+                {note.name}
+                </Link>
+                {note.modified}
+                </li>
+            })}
+            </div>
+        )
+    }
+}
+
+export default Notes;
