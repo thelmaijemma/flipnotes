355630aad48d404eb1740492e98e5aaad507e27a
thelmaijemma <okoli.thelma@gmail.com>
2021-01-26 00:36:26 +0000

    working app with custom date retrieval function

--- src/App.js
+++ src/App.js
@@ -1,52 +1,123 @@
+import React, {Component} from 'react';
+import {Route, Link} from 'react-router-dom';
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import NoteListNav from './NoteListNav/NoteListNav';
+import NotePageNav from './NotePageNav/NotePageNav';
+import NoteListMain from './NoteListMain/NoteListMain';
+import NotePageMain from './NotePageMain/NotePageMain';
+import staticNotes from './staticNotes';
+import {getNotesForFolder, findNote, findFolder} from './staticDisplayAndFilter';
 import './App.css';
-import React, {Component } from 'react';
-import MainSidebar from './MainSidebar';
-import { Route, Switch  } from 'react-router-dom';
-import SideBar from './SideBar';
-import Header from './Header';
-import SidebarFolders from './SidebarFolders';
-import Main from './Main';
-import Note from './Note';
-import FOLDERS from'./Folders';
-import Notes from './Notes';
+{/*npm i --save @fortawesome/fontawesome-svg-core
+  npm install --save @fortawesome/free-solid-svg-icons
+  npm install --save @fortawesome/react-fontawesome
 
+in index:
+import { library } from '@fortawesome/fontawesome-svg-core'
+import {
+  faPlus, faChevronLeft, faTrashAlt, faCheckDouble
+} from '@fortawesome/free-solid-svg-icons'
+
+before render:
+library.add(faPlus, faChevronLeft, faTrashAlt, faCheckDouble)
+
+*/}
 
 class App extends Component {
-  render(){
-  return (
-    <div>
-      <Header></Header>
-    <SideBar>
-      <Route exact path='/' component={MainSidebar} />
-      <Route path='/folder/:folderId' 
-      component={SidebarFolders} />
-    </SideBar>
-    <Main>
-      <Route exact path='/' component={Main} />
-      <Route path='/note/:noteId' 
-      render={(routeProps) => 
-        <Notes
-        noteId= {FOLDERS.notes.find(n =>
-        n.id === routeProps.match.params.noteId)}
-     />
-        }
-      />
-    </Main>
-  </div>
-  );
-  }
+    state = {
+        notes: [],
+        folders: []
+    };
+
+    componentDidMount() {
+        // fake date loading from API call
+        setTimeout(() => this.setState(staticNotes), 600);
+    }
+
+    renderNavRoutes() {
+        const {notes, folders} = this.state;
+        return (
+            <>
+                {['/', '/folder/:folderId'].map(path => (
+                    <Route
+                        exact
+                        key={path}
+                        path={path}
+                        render={routeProps => (
+                            <NoteListNav
+                                folders={folders}
+                                notes={notes}
+                                {...routeProps}
+                            />
+                        )}
+                    />
+                ))}
+                <Route
+                    path="/note/:noteId"
+                    render={routeProps => {
+                        const {noteId} = routeProps.match.params;
+                        const note = findNote(notes, noteId) || {};
+                        const folder = findFolder(folders, note.folderId);
+                        return <NotePageNav {...routeProps} folder={folder} />;
+                    }}
+                />
+                <Route path="/add-folder" component={NotePageNav} />
+                <Route path="/add-note" component={NotePageNav} />
+            </>
+        );
+    }
+
+    renderMainRoutes() {
+        const {notes, folders} = this.state;
+        return (
+            <>
+                {['/', '/folder/:folderId'].map(path => (
+                    <Route
+                        exact
+                        key={path}
+                        path={path}
+                        render={routeProps => {
+                            const {folderId} = routeProps.match.params;
+                            const notesForFolder = getNotesForFolder(
+                                notes,
+                                folderId
+                            );
+                            return (
+                                <NoteListMain
+                                    {...routeProps}
+                                    notes={notesForFolder}
+                                />
+                            );
+                        }}
+                    />
+                ))}
+                <Route
+                    path="/note/:noteId"
+                    render={routeProps => {
+                        const {noteId} = routeProps.match.params;
+                        const note = findNote(notes, noteId);
+                        return <NotePageMain {...routeProps} note={note} />;
+                    }}
+                />
+            </>
+        );
+    }
+
+    render() {
+        return (
+            <div className="App">
+                <nav className="App__nav">{this.renderNavRoutes()}</nav>
+                <header className="App__header">
+                    <h1>
+                        <Link to="/">Noteful</Link>{' '}
+                        <FontAwesomeIcon icon="check-double" />
+                    </h1>
+                </header>
+                <main className="App__main">{this.renderMainRoutes()}</main>
+            </div>
+        );
+    }
 }
 
 export default App;
 
-
-{/*    <Sidebar>
-      <Route path='/' component={MainSidebar} />
-      <Route path='/folder' component={SidebarFolders} />
-      <Route path='/note' component={SidebarNotes} />
-    </Sidebar>
-    <Main>
-      <Route path='/' component={MainMain} />
-      <Route path='/folder' component={MainFolders} />
-      <Route path='/note' component={MainNotes} />
-    </Main> */}
--- src/index.js
+++ src/index.js
@@ -1,20 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import { BrowserRouter } from 'react-router-dom';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import {
+  faPlus, faChevronLeft, faTrashAlt, faCheckDouble
+} from '@fortawesome/free-solid-svg-icons'
+import { BrowserRouter } from 'react-router-dom'
+import './index.css'
+import App from './App'
 
-// npm install react-router-dom
+library.add(faPlus, faChevronLeft, faTrashAlt, faCheckDouble)
 
 ReactDOM.render(
   <BrowserRouter>
     <App />
   </BrowserRouter>,
   document.getElementById('root')
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+)
